38b51bd01e5f7db512c42b98fdae659772d41bd1
SivaChaitanyaS <sivachaitanya37@gmail.com>
2016-12-19 18:47:35 +0530

    UnityContainer_Example added to MyPractices

---
+++ MyPractices/UnityContainer_Example/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityContainer_Example
+{
+    class Program
+    {
+        public interface ICompany
+        {
+            void ShowSalary();
+        }
+        public class Company : ICompany
+        {
+            public void ShowSalary()
+            {
+                Console.WriteLine("Your salary is 1.25 Lakhs ");
+            }
+        }
+        public interface IEmployee
+        {
+
+        }
+        public class Employee : IEmployee
+        {
+            private ICompany _company;
+            [InjectionConstructor]
+            public Employee(ICompany company)
+            {
+                _company = company;
+            }
+
+            public void DisplaySalary()
+            {
+                _company.ShowSalary();
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Simple Approach
+            //Employee emp = new Employee(new Company());
+            //emp.DisplaySalary();
+
+            // Approach with Unity Container
+            IUnityContainer unity = new UnityContainer();
+            unity.RegisterType<ICompany, Company>();
+
+            Employee emp = unity.Resolve<Employee>();
+            emp.DisplaySalary();
+
+
+            Console.ReadLine();
+        }
+    }
+}
