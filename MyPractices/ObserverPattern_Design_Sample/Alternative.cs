d1d19f63f8a171467371eacc6070d6a0f0f50e0a
SivaChaitanyaS <sivachaitanya37@gmail.com>
2016-12-21 19:59:44 +0530

    SOLID , Observer Pattern added to MyPractices

---
+++ MyPractices/ObserverPattern_Design_Sample/Alternative.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern_Design_Sample
+{
+    class Alternative
+    {
+        public static void Main(string[] args)
+        {
+            IBM ibm = new ObserverPattern_Design_Sample.IBM(100, "IBM");
+            ibm.Attach(new Investor("Investor1"));
+            ibm.Attach(new Investor("Investor2"));
+            ibm.Attach(new Investor("Investor3"));
+
+            ibm.Price = 200;
+            Console.ReadLine();
+
+        }
+    }
+    class IBM : Stock
+    {
+        public IBM(double price, string name) : base(price, name)
+        {
+        }
+    }
+    class Stock
+    {
+        private double _price;
+        private string _name;
+        public List<Investor> _investors = new List<Investor>();
+        public Stock(double price, string name)
+        {
+            _price = price;
+            _name = name;
+        }
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    Notify();
+                }
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public void Attach(Investor investor)
+        {
+            _investors.Add(investor);
+        }
+        public void Detach(Investor investor)
+        {
+            _investors.Remove(investor);
+        }
+        public void Notify()
+        {
+            foreach (Investor investor in _investors)
+            {
+                investor.Update(this);
+            }
+        }
+    }
+    class Investor
+    {
+        private string _name;
+        public Stock Stock { get; set; }
+        public Investor(string name)
+        {
+            _name = name;
+        }
+        public void Update(Stock stock)
+        {
+            Console.WriteLine($"Given stock :{stock.Name}: update to :{stock.Price}: and notified to :{_name}: investor");
+        }
+    }
+}
---
+++ MyPractices/SOLIDPrinciples_Sample/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples_Sample
+{
+    interface IDatabase
+    {
+        void Add();
+    }
+    //'I'
+    interface IDatabaseV1
+    {
+        void Read();
+    }
+    interface IDiscount
+    {
+        double GetDiscount(double cost);
+    }
+
+    interface ILogger
+    {
+        void LogMessage(string message);
+    }
+    class Customer : IDatabase, IDiscount
+    {
+        public ILogger iLog;
+        public Customer()
+        {
+
+        }
+        public Customer(ILogger log)
+        {
+            iLog = log;
+        }
+        public int TypeOfCustomer { get; set; }
+        GeneralLogger log = new GeneralLogger();
+        public virtual void Add()
+        {
+            try
+            {
+                // Some Db operation goes here
+                throw new Exception("Test Exception");
+            }
+            catch (Exception ex)
+            {
+                // Violation of 'S' Single Responsibility
+                //System.IO.File.WriteAllText(@"E:\DoNotDelete_Siva_All\Source\Repos\NewRepo\MyPractices\SOLIDPrinciples_Sample\SOLIDPrinciples_Sample_Files\Exceptions.txt"
+                //, ex.Message);
+                log.LogMessage(ex.Message);
+
+                //'D'
+                iLog.LogMessage(ex.Message);
+            }
+        }
+
+        // Violation of 'O' open close 
+        //public virtual double GetDiscount(double cost)
+        //{
+        //    if (TypeOfCustomer == 1)
+        //    {
+        //        return cost - 100;
+        //    }
+        //    else
+        //    {
+        //        return cost - 50;
+        //    }
+        //}
+        //'O'
+        public virtual double GetDiscount(double cost) { return cost; }
+    }
+    //'I'
+    class CustomerWithRead : IDatabaseV1
+    {
+        public void Read()
+        {
+            //;
+        }
+    }
+    //'O'
+    //class GoldCustomer : Customer
+    class GoldCustomer : Customer
+    {
+        public override double GetDiscount(double cost)
+        {
+            return base.GetDiscount(cost) - 100;
+        }
+    }
+    //'O'
+    //class SilverCustomer : Customer
+    class SilverCustomer : Customer
+    {
+        public override double GetDiscount(double cost)
+        {
+            return base.GetDiscount(cost) - 50;
+        }
+    }
+
+    //'L'
+    //class Enquire : Customer
+    class Enquire : IDiscount
+    {
+        public double GetDiscount(double cost)
+        {
+            return cost - 5;
+        }
+    }
+
+    //'D'
+    class GeneralLogger : ILogger
+    {
+        // As part of 'S' Single Responsibility 
+        public void LogMessage(string message)
+        {
+            System.IO.File.WriteAllText(@"E:\DoNotDelete_Siva_All\Source\Repos\NewRepo\MyPractices\SOLIDPrinciples_Sample\SOLIDPrinciples_Sample_Files\Exceptions.txt"
+                                                , message);
+        }
+    }
+    //'D'
+    class FileLogger : ILogger
+    {
+        public void LogMessage(string message)
+        {
+            // Log to file
+        }
+    }
+    //'D'
+    class EmailLogger : ILogger
+    {
+        public void LogMessage(string message)
+        {
+            // Log to Email
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Customer cust = new Customer();
+            //cust.Add();
+
+            //'L'
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new GoldCustomer());
+            customers.Add(new SilverCustomer());
+            //'L' will no longer be of type Customer as it is IDiscount type
+            //customers.Add(new Enquire());
+
+            foreach (Customer cust in customers)
+            {
+                //'L' hits exception as Add() is not implemented for Enquire class
+                cust.Add();
+            }
+
+
+            //'I'
+            IDatabase db = new Customer();
+            db.Add();
+            //'I'
+            IDatabaseV1 dbV1 = new CustomerWithRead();
+            dbV1.Read();
+
+
+            Console.ReadLine();
+        }
+    }
+}
